237490c458e9dd60c89451019287fab075a9ce11
JacProsser <jacleighp@gmail.com>
2021-06-05 00:17:37 +0100

    .

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,4 @@
-
-
- export default ({
+export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
