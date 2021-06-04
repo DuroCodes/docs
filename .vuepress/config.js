3f82a6bebbb1b165b2d2911b981e1d80f6b04be4
jac <48368615+JacProsser@users.noreply.github.com>
2021-06-04 22:54:58 +0100

    Add files via upload

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,1 +1,76 @@
+const { description } = require('../../package')
 
+module.exports = {
+
+  title: 'discord-buttons',
+  description: description,
+
+  head: [
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "https://i.imgur.com/m57ng0P.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "https://i.imgur.com/0Dfw71u.png" }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  themeConfig: {
+    smoothScroll: true,
+    searchPlaceholder: 'Search',
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: true,
+    nav: [
+      {
+        text: 'Documentation',
+        link: '/documentation/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/AngeloCore/discord-buttons'
+      },
+    ],
+    sidebar: {
+      '/documentation/': [
+        {
+          title: 'Getting Started',
+          collapsable: false,
+          children: [
+            './getting-started/installation.md',
+            './getting-started/setup.md',
+          ]
+        },
+        {
+          title: 'Sending Buttons',
+          collapsable: false,
+          children: [
+            './sending-buttons/channel-send',
+            './sending-buttons/message-edit',
+            './sending-buttons/message-reply'
+          ]
+        },
+        {
+          title: 'Contructors',
+          collapsable: false,
+          children: [
+            './constructors/message-button',
+            './constructors/message-action-row',
+          ]
+        },
+      ],
+    }
+  },
+
+  theme: 'yuu',
+
+  markdown: {
+    lineNumbers: true,
+  },
+
+  plugins: [
+    '@xiaopanda/vuepress-plugin-code-copy',
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ .vuepress/enhanceApp.js
@@ -0,0 +1,20 @@
+import VueDiscordMessage from 'vue-discord-message';
+
+export default ({ Vue }) => {
+  Vue.use(VueDiscordMessage, {
+    avatars: {
+      discordbuttons: require('src/.vuepress/assets/angelo.gif'),
+    },
+    profiles: {
+      user: {
+        author: 'Angelo',
+        avatar: 'discordbuttons',
+      },
+      bot: {
+        author: 'discord-buttons',
+        avatar: 'green',
+        bot: true,
+      },
+    },
+  });
+};
