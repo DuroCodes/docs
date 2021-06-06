97421f501eb9a09c5bd89c103df011df8d9712bc
JacProsser <jacleighp@gmail.com>
2021-06-06 20:45:22 +0100

    d

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,11 @@ module.exports = {
   description: 'Discord.js buttons',
 
   head: [
+    ['meta', { charset: 'utf-8' }],
+		['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "https://i.imgur.com/m57ng0P.png" }],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "https://i.imgur.com/0Dfw71u.png" }],
     ['meta', { name: 'theme-color', content: '#2196f3' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
 		['meta', { property: 'og:title', content: 'Get Started' }],
 		['meta', { property: 'og:description', content: 'Discord.js buttons' }],
 		['meta', { property: 'og:type', content: 'website' }],
