0f124d29f3f420f0e095854d2b60a4bc12a45075
JacProsser <jacleighp@gmail.com>
2021-07-03 15:49:34 +0100

    h

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   title: "discord-buttons",
-  base: "/#/",
+  base: "/docs/",
   head: [
     ['meta', { charset: 'utf-8' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
